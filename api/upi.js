6e7a3a4e9520636c416f8152f7c8ab7ab439aa11
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-06-20 17:01:23 +0530

    Update upi.js

--- api/upi.js
+++ api/upi.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
@@ -7,9 +7,25 @@ export default async function handler(req, res) {
 
   const { query } = req.body;
 
-  return res.status(200).json({
-    success: true,
-    query,
-    message: "upi endpoint working"
+  if (!query) {
+    return res.status(400).json({
+      error: "Missing query"
+    });
+  }
+
+ try {
+  const response = await fetch(
+    `https://osint-bronx-ultra-2-0.onrender.com/api/key-bronx/upi?key=op&upi=${encodeURIComponent(query)}`
+  );
+
+  const text = await response.text();
+console.log(text);
+
+return res.status(response.status).send(text);
+
+} catch (err) {
+  return res.status(500).json({
+    error: err.message
   });
-}
+ }
+};
